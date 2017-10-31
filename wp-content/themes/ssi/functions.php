d0279e2468240e4edca538f8a23fdc655c90ee59
Matt <matt@mspacecreative.com>
2017-10-31 16:19:01 -0300

    c

--- wp-content/themes/ssi/functions.php
+++ wp-content/themes/ssi/functions.php
@@ -2,6 +2,9 @@
 
 function footer_scripts() 
 {
+	wp_register_script('jquery-cdn', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js', array('jquery'), null, true);
+	wp_enqueue_script('jquery-cdn');
+	
 	wp_register_script('slider', 'http://responsiveslides.com/responsiveslides.min.js', array('jquery'), null, true);
 	wp_enqueue_script('slider');
 	
@@ -11,9 +14,4 @@ function footer_scripts()
 	wp_register_script('fontawesome', 'https://use.fontawesome.com/6ccd600e51.js', array('jquery'), null, true);
 	wp_enqueue_script('fontawesome');
 }
-function main_styles() 
-{
-	wp_enqueue_style('lb-style', get_template_directory_uri() . '/css/lightbox.css', false, '1.0', 'all');
-}
-add_action('wp_enqueue_scripts', 'main_styles');
 add_action('init', 'footer_scripts');
